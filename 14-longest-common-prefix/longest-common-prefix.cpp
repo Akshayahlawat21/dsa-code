59d20d237be3250ecd5b9d6b04ab7df7e5cdedf9
akshay ahlawat <akshayahalawat568@gmail.com>
2026-04-27 17:09:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (62.34%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+        int n = strs.size();
+
+        string ans = "";
+
+        for (int i = 0; i < strs[0].size();
+             i++) { // ye  1 comple word pr chlega
+
+            char ch = strs[0][i];
+
+            for (int j = 1; j < n; j++) { // ye 2nd word se baki words pr chelga
+
+                if (i >= strs[j].size() || strs[j][i] != ch) {
+                    return ans;
+                }
+                
+            }
+              ans.push_back(ch);
+        }
+        return  ans;
+    }
+};
