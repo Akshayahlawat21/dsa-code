5a8f6899763deddee404235a5877ce759624cd06
akshay ahlawat <akshayahalawat568@gmail.com>
2026-05-22 16:37:46 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,44 +1,68 @@
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+
+//         int i = 0;
+//         int j = nums.size() - 1;
+
+//         while (i <= j) {
+
+//             int mid = i + (j - i) / 2;
+
+        
+//             if (nums[mid] == target) {
+//                 return mid;
+//             }
+
+
+//             if( nums[i]<= nums[mid]){
+//                 if( nums[i]<= target && target < nums[mid]){
+//                     j = mid -1;
+//                 }
+//                 else {
+//                     i = mid+1;
+//                 }
+//             }
+
+//             else{
+//                 if( nums[mid] < target && target <= nums[j]){
+//                     i = mid +1;
+//                 }
+//                 else {
+//                     j = mid-1;
+//                 }
+//             }
+//         }
+//         return -1;
+
+//     }
+// };
+
+     
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        int lo=0;
+        int hi=nums.size()-1;
+        while(lo<=hi){
 
-        int i = 0;
-        int j = nums.size() - 1;
+            int mid=lo+(hi-lo)/2;
 
-        while (i <= j) {
-
-            int mid = i + (j - i) / 2;
-
-        
-            if (nums[mid] == target) {
-                return mid;
+            if(nums[mid]==target)return mid;
+            
+            if(nums[mid]<nums[hi]){
+                if(target>nums[mid] && target<=nums[hi])lo=mid+1;
+                else hi=mid-1;
             }
-
-
-            if( nums[i]<= nums[mid]){
-                if( nums[i]<= target && target < nums[mid]){
-                    j = mid -1;
-                }
-                else {
-                    i = mid+1;
-                }
-            }
-
             else{
-                if( nums[mid] < target && target <= nums[j]){
-                    i = mid +1;
+                if(target<nums[mid] && target>=nums[lo]){
+                    hi=mid-1;
                 }
-                else {
-                    j = mid-1;
-                }
+                else lo=mid+1;
             }
         }
         return -1;
-
     }
 };
-
-     
-
 
           
