5bb099b9353a738c34b1fb4d8bac2053f98dca77
akshay ahlawat <akshayahalawat568@gmail.com>
2026-06-07 20:28:51 +0530

    Time: 103 ms (93.30%) | Memory: 259.2 MB (92.02%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        int n = descriptions.size();
+        TreeNode* root;
+        unordered_map<int, TreeNode*> mp;
+
+        for (int i = 0; i < n; i++) {
+            int curr = descriptions[i][1];
+            mp[curr] = new TreeNode(curr);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int parent = descriptions[i][0];
+            int curr = descriptions[i][1];
+            bool isLeft = descriptions[i][2];
+
+            // find root
+            if (mp.find(parent) == mp.end()) {
+                mp[parent] = new TreeNode(parent);
+                root = mp[parent];
+            }
+
+            TreeNode* parentNode = mp[parent];
+            if (isLeft) {
+                parentNode->left = mp[curr];
+            } else {
+                parentNode->right = mp[curr];
+            }
+
+            mp[parent] = parentNode;
+        }
+
+        return root;
+    }
+};
