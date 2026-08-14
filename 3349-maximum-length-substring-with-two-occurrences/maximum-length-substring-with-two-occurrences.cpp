d5504a5f902663010806127e7aee04b13d55bc32
akshay ahlawat <akshayahalawat568@gmail.com>
2026-08-14 20:58:30 +0530

    Time: 35 ms (7.23%) | Memory: 19.1 MB (10.29%) - LeetSync

---
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+
+        int n = s.size();
+
+        int ans = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            unordered_map<int, int> mp;
+            for (int j = i; j < n; j++) {
+                mp[s[j]]++;
+                if (mp[s[j]] > 2)
+                    break;
+
+                ans = max(ans, j - i + 1);
+            }
+        }
+        return ans;
+    }
+};
