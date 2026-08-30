2a80177790b8cac79ae0aee36f8ffd759929e45c
akshay ahlawat <akshayahalawat568@gmail.com>
2026-08-30 16:42:50 +0530

    Time: 7 ms (12.79%) | Memory: 9.9 MB (12.52%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,59 +1,95 @@
+// class Solution {
+// public:
+//     string shortestBeautifulSubstring(string s, int k) {
+
+//         vector<string> ans;
+
+//         // Generate all substrings
+//         for (int i = 0; i < s.length(); i++) {
+
+//             string temp = "";
+
+//             for (int j = i; j < s.length(); j++) {
+
+//                 temp += s[j];
+
+//                 ans.push_back(temp);
+//             }
+//         }
+
+//         // Keep only strings having exactly k ones
+//         vector<string> result;
+
+//         for (string str : ans) {
+
+//             int count = 0;
+
+//             for (char ch : str) {
+
+//                 if (ch == '1') {
+//                     count++;
+//                 }
+//             }
+
+//             if (count == k) {
+//                 result.push_back(str);
+//             }
+//         }
+
+//         // No beautiful substring
+//         if (result.empty()) {
+//             return "";
+//         }
+
+//         // Find shortest, and if same length,
+//         // lexicographically smallest
+//         string smallest = result[0];
+
+//         for (string str : result) {
+
+//             if (str.length() < smallest.length()) {
+
+//                 smallest = str;
+//             }
+//             else if (str.length() == smallest.length() && str < smallest) {
+
+//                 smallest = str;
+//             }
+//         }
+
+//         return smallest;
+//     }
+// };
+
+
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
 
-        vector<string> ans;
+        string smallest = "";
 
-        // Generate all substrings
         for (int i = 0; i < s.length(); i++) {
 
             string temp = "";
+            int count = 0;
 
             for (int j = i; j < s.length(); j++) {
 
                 temp += s[j];
 
-                ans.push_back(temp);
-            }
-        }
-
-        // Keep only strings having exactly k ones
-        vector<string> result;
-
-        for (string str : ans) {
-
-            int count = 0;
-
-            for (char ch : str) {
-
-                if (ch == '1') {
+                if (s[j] == '1') {
                     count++;
                 }
-            }
 
-            if (count == k) {
-                result.push_back(str);
-            }
-        }
+                if (count == k) {
 
-        // No beautiful substring
-        if (result.empty()) {
-            return "";
-        }
+                    if (smallest == "" ||
+                        temp.length() < smallest.length() ||
+                        (temp.length() == smallest.length() && temp < smallest)) {
 
-        // Find shortest, and if same length,
-        // lexicographically smallest
-        string smallest = result[0];
-
-        for (string str : result) {
-
-            if (str.length() < smallest.length()) {
-
-                smallest = str;
-            }
-            else if (str.length() == smallest.length() && str < smallest) {
-
-                smallest = str;
+                        smallest = temp;
+                    }
+                }
             }
         }
 
